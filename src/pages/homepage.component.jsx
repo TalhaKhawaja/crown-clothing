8bec4c2048f313f13113eb20c6fe3390f6dcb209
TalhaKhawaja <talhanaik@gmail.com>
2020-12-17 15:23:17 -0500

    Added components and stylized the menu-item component

---
+++ src/pages/homepage.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Directory from '../components/directory.component';
+import '../assets/styles/homepage.scss';
+
+const HomePage = () => (
+    <div className='homepage'>
+        <Directory></Directory>
+    </div>
+);
+
+export default HomePage;
