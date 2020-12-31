7e046597cda21b92c2e69a3a986b3263918e32c3
TalhaKhawaja <talhanaik@gmail.com>
2020-12-31 14:03:35 -0500

    Added directory and shop reducer and selectors to use on the directory and shop component, also added persistant state, local storage to the application

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,27 +1,21 @@
 import React from 'react';
-import SHOP_DATA from './shop.data.js'
 import CollectionPreview from '../../components/collection-preview.component';
+import { createStructuredSelector } from 'reselect';
+import {selectCollections} from '../../redux/shop/shop.selectors';
+import { connect } from 'react-redux';
 
-class ShopPage extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            collections: SHOP_DATA
-        };
-    }
-
-    render() {
-        const {collections} = this.state;
-        return (<div className='shop-page'> 
+const ShopPage = ({ collections }) => (
+    <div className='shop-page'>
         {
-            collections.map(({id, ...otherCollectionProps}) => (
+            collections.map(({ id, ...otherCollectionProps }) => (
                 <CollectionPreview key={id} {...otherCollectionProps}></CollectionPreview>
             ))
         }
-        </div>
-        );
-    }
-}
+    </div>
+);
 
-export default ShopPage;
+const mapStateToProps = createStructuredSelector({
+    collections: selectCollections
+});
+
+export default connect(mapStateToProps)(ShopPage);
