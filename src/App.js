7d49dc0c9e672c50f186864d5800522a342009c2
TalhaKhawaja <talhanaik@gmail.com>
2021-09-05 15:28:21 -0400

    refactored the google sign in and email sign in using sagas

--- src/App.js
+++ src/App.js
@@ -5,9 +5,7 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header.component';
 import SignInAndSignOutPage from './components/sign-in-and-sign-up.component';
-import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { connect } from 'react-redux';
-import { setCurrentUser } from './redux/user/user.action';
 import { selectCurrentUser } from './redux/user/user.selector';
 import { createStructuredSelector } from 'reselect';
 import CheckOutPage from './pages/checkout/checkout.component';
@@ -16,7 +14,6 @@ class App extends React.Component {
   unsubscribleFromAuth = null;
 
   componentDidMount() {
-    const { setCurrentUser } = this.props;
 
     // this.unsubscribleFromAuth = auth.onAuthStateChanged(async userAuth => {
     //   if (userAuth) {
@@ -56,8 +53,4 @@ const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser
 });
 
-const mapDispatchToProps = dispatch => ({
-  setCurrentUser: user => dispatch(setCurrentUser(user))
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(mapStateToProps)(App);
